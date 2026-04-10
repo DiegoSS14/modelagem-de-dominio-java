56b583af3efde8500e8432f6d4a2cf6109c8e384
unknown <sousadiego533@gmail.com>
2026-04-10 20:21:01 -0300

    first commit

---
+++ backend/src/main/java/com/diego/user/User.java
@@ -0,0 +1,21 @@
+package com.diego.user;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class User {
+    String id;
+    String name;
+    String email;
+    String password;
+}

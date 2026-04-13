2c5c96f678d3e6e45711c00a2020556b072e7f58
unknown <sousadiego533@gmail.com>
2026-04-13 14:21:23 -0300

    test: add more tests to validate User model

--- backend/src/test/java/com/diego/anemico/user/UserTest.java
+++ backend/src/test/java/com/diego/anemico/user/UserTest.java
@@ -1,7 +1,10 @@
 package com.diego.anemico.user;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +18,7 @@ public class UserTest {
         assertEquals("diego@gmail.com", user.getEmail());
         assertEquals("12345678", user.getPassword());
     }
-
+    
     @Test
     void deveCriarUmUsuarioComTodosOsCamposVazios() {
         User user = new User(" ", " ", " ", " ");
@@ -24,7 +27,7 @@ public class UserTest {
         assertEquals(" ", user.getEmail());
         assertEquals(" ", user.getPassword());
     }
-
+    
     @Test
     void deveCriarUmUsuarioNulo() {
         User user = new User();
@@ -32,5 +35,43 @@ public class UserTest {
         assertNull(user.getName());
         assertNull(user.getEmail());
         assertNull(user.getPassword());
+    }
+    
+    @Test 
+    void devecriarUmUsuarioVazioEDefinirOsAtributosComSetters() {
+        User user = new User();
+        user.setId("1");
+        user.setName("Diego");
+        user.setEmail("diego@gmail.com");
+        user.setPassword("abcdefgh");
+        
+        assertNotNull(user);
+    }
+    
+    @Test 
+    void deveCompararDoisUsuariosDiferentes(){
+        User user = new User("1", "Diego", "diego@gmail.com", "12345678");
+        User user2 = new User("2", "Diego", "diego@gmail.com", "12345678");
+        assertNotEquals(user, user2);
+    }
+
+    @Test 
+    void deveCompararDoisUsuariosIguaisEmEquals(){
+        User user = new User("1", "Diego", "diego@gmail.com", "12345678");
+        assertTrue(user.canEqual(user));
+    }
+
+    @Test 
+    void deveCompararDoisUsuariosIguais(){
+        User user = new User("1", "Diego", "diego@gmail.com", "12345678");
+        User user2 = new User("1", "Diego", "diego@gmail.com", "12345678");
+        assertEquals(user, user2);
+    }
+
+    @Test 
+    void deveCompararOHashcodeDoObjeto(){
+        User user = new User("1", "Diego", "diego@gmail.com", "12345678");
+        User user2 = new User("1", "Diego", "diego@gmail.com", "12345678");
+        assertEquals(user.hashCode(), user2.hashCode());
     }
 }
